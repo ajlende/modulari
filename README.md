0b19a4f8b906279f283476040a00b801a17b40a0
Alex Lende <ajlende@gmail.com>
2016-05-30 23:37:33 -0500

    Restructuring of the app
    Rectory structure based on `Aluxian/electron-superkit`
    When things get more complex, it might be useful to look at that repo again.

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Simple client for [Mopidy](https://www.mopidy.com) built with [Electron](http://
 
 This project is in its infancy, so many things will be broken or not exist at the moment.
 
-Boilerplate was taken from [edge/electron-cycle-media](https://github.com/edge/electron-cycle-media).
-
 ## Usage
 ```sh
-$ npm i -g electron-prebuilt
-$ npm i
-$ electron .
+$ npm install -g electron-prebuilt
+$ npm install
+$ npm start
 ```
---
+++ src/scripts/browser/app-window.js
@@ -0,0 +1,76 @@
+import {BrowserWindow, shell} from 'electron';
+import {EventEmitter} from 'events';
+
+class AppWindow extends EventEmitter {
+
+  /**
+   * Create a browser window based off of some default settings.
+   *
+   * @param {Object} options
+   */
+  constructor(options) {
+    super();
+
+    const defaults = {
+      'width': 1280,
+      'height': 720,
+      'web-preferences': {
+        'webaudio': true,
+        'web-security': false,
+        'use-content-size': true,
+        'subpixel-font-scaling': true,
+        'direct-write': true,
+        'plugins': true
+      }
+    };
+
+    this.settings = Object.assign(defaults, options);
+    this.window = this.createBrowserWindow(this.settings);
+  }
+
+  /**
+   * Create a BrowserWindow with the given settings.
+   *
+   * @param {Object} settings
+   * @return {BrowserWindow}
+   */
+  createBrowserWindow(settings) {
+    const browserWindow = new BrowserWindow(settings);
+
+    // Open urls in an external browser
+    browserWindow.webContents.on('new-window', function (event, url) {
+      event.preventDefault();
+      shell.openExternal(url);
+    });
+
+    return browserWindow;
+  }
+
+  /**
+   * Load the target url inside the window.
+   *
+   * @param {String} targetUrl
+   */
+  loadURL(targetUrl) {
+    console.log(`loading: ${this.window.loadURL}`);
+    this.window.loadURL(targetUrl);
+  }
+
+  /**
+   * Make the window visible and focus it.
+   */
+  show() {
+    this.window.show();
+  }
+
+  /**
+   * Close the window.
+   */
+  close() {
+    this.window.close();
+    this.window = null;
+  }
+
+}
+
+export default AppWindow;
